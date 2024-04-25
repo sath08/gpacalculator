4d52ff3226a03c055cd8ba5c9cf6074162312aa6
sath08 <167156060+sath08@users.noreply.github.com>
2024-04-24 18:32:10 -0700

    Update GPAResourcesHandler.java

--- GPAServer/src/handlers/GPAResourcesHandler.java
+++ GPAServer/src/handlers/GPAResourcesHandler.java
@@ -16,13 +16,13 @@ import java.nio.file.Paths;
  */
 public class GPAResourcesHandler implements HttpHandler {
     
-	/**
+        /**
 	 * Method for handling GET requests for the resources and calls corresponding method
 	 * and accounting for exceptions. 
 	 * 
 	 * @param exchange		Http Exchange Object
 	 * 
-	 */
+	*/
 	public void handle(HttpExchange exchange) {
         if (exchange.getRequestMethod().equals("GET")) {
             try {
@@ -37,14 +37,14 @@ public class GPAResourcesHandler implements HttpHandler {
         }
     }
 
-	/**
-	 * Method for handling GET requests. Retrieves the resource file name 
-	 * specified in query and writes the contents of the file to Outputstream
-	 * 
-	 * @param exchange	Http Exchange Object
-	 * @throws IOException	URISyntaxException 
-	 * 
-	 */
+   /**
+    * Method for handling GET requests. Retrieves the resource file name 
+    * specified in query and writes the contents of the file to Outputstream
+    * 
+    * @param exchange	Http Exchange Object
+    * @throws IOException	URISyntaxException 
+    * 
+    */
     private void handleGetRequest(HttpExchange exchange) throws IOException, URISyntaxException {
         String fileName = exchange.getRequestURI().getQuery(); // fileName contains URL past '?'
         byte[] bytes = readFile(fileName); // Byte array reading the html file
@@ -55,14 +55,14 @@ public class GPAResourcesHandler implements HttpHandler {
         os.close();
     }
 
-	/**
-	 * Helper method for reading files and returning byte array.
-	 * 
-	 * @param fileName		filename to read
-	 * @return byte[]		Byte array for the contents of the file
-	 * @throws IOException	URISyntaxException 
-	 * 
-	 */
+    /**
+     * Helper method for reading files and returning byte array.
+     * 
+     * @param fileName		filename to read
+     * @return byte[]		Byte array for the contents of the file
+     * @throws IOException	URISyntaxException 
+     * 
+    */
     private byte[] readFile (String fileName) throws IOException, URISyntaxException {
     	URI url  = this.getClass().getClassLoader().getResource(fileName).toURI();
         return Files.readAllBytes(Paths.get(url));
