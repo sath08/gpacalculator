2c1ec92fa0f1eb89f0a18633353d3dd09b20225c
sath08 <167156060+sath08@users.noreply.github.com>
2024-04-24 19:37:30 -0700

    Update TranscriptHandler.java

--- GPAServer/src/handlers/TranscriptHandler.java
+++ GPAServer/src/handlers/TranscriptHandler.java
@@ -13,26 +13,43 @@ import com.sun.net.httpserver.HttpHandler;
 import domain.StudentRecord;
 import server.Utils;
 
+/**
+ * Handler for /transcript context responsible for handling the transcript. 
+ * GET method is supported. 
+ * 
+ */
 public class TranscriptHandler implements HttpHandler {
 
+	/**
+	 * Handler method for transcript. If the method is GET, it
+	 * calls handleGetRequest. 
+	 * 
+	 */
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
 		if (exchange.getRequestMethod().equals("GET")) {
-            try {
+            		try {
 				handleGetRequest(exchange);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-        }
+        	}
 	}
 
+	/**
+	 * Implementation for handling GET for transcript. Retrieves the saved StudentRecord 
+	 * for the user and returns the corresponding data in JSON format.
+	 * 
+	 * @param exchange - Http Exchange Object
+	 * @throws Exception
+	 */
 	private void handleGetRequest(HttpExchange exchange) throws Exception {
-        // Retrieve identifier from URL
+       		 // Retrieve identifier from URL
 		String format = exchange.getRequestURI().getQuery();
 		// Find CSV file for identifier, read file content, and covert to java object
 		String identifier = Utils.readIdentifier();
 
-        // Find CSV file for identifier, read file content, and covert to java object
+        	// Find CSV file for identifier, read file content, and covert to java object
 		StudentRecord studentRecord = readFile("C:\\Users\\sathk\\OneDrive\\Desktop\\gpacalculator\\GPAServer\\data\\Transcript_" + identifier + ".csv");
 
 		byte[] encoded = null;
@@ -50,23 +67,29 @@ public class TranscriptHandler implements HttpHandler {
 			exchange.getResponseHeaders().set("Content-Disposition", "attachment; filename=" + "Transcript.csv");
 			exchange.sendResponseHeaders(200, encoded.length);
 		}
-        OutputStream os = exchange.getResponseBody();
-        os.write(encoded);
-        os.close();
+        	OutputStream os = exchange.getResponseBody();
+       		os.write(encoded);
+        	os.close();
 	}
-	
+
+	/**
+    	* Helper method to read the StudentRecord from the disk
+     	* @param path
+     	* @return StudentRecord
+     	* @throws Exception
+     	*/
 	private StudentRecord readFile(String path) throws Exception{
-        FileInputStream fileIn = new FileInputStream(path);
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        StudentRecord studentRecord = (StudentRecord) in.readObject();
-        in.close();
-        fileIn.close();
-        return studentRecord;
+        	FileInputStream fileIn = new FileInputStream(path);
+        	ObjectInputStream in = new ObjectInputStream(fileIn);
+        	StudentRecord studentRecord = (StudentRecord) in.readObject();
+        	in.close();
+        	fileIn.close();
+        	return studentRecord;
 	}
 	
-    private String readIdentifier(String path) throws Exception{
-        FileInputStream fileIn = new FileInputStream(path);
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        return(String) in.readObject();
-    }
+    	private String readIdentifier(String path) throws Exception{
+        	FileInputStream fileIn = new FileInputStream(path);
+        	ObjectInputStream in = new ObjectInputStream(fileIn);
+        	return(String) in.readObject();
+    	}
 }
