26b3763b8ba1fb25ce7a59b9a0f90424e9345d8f
sath08 <167156060+sath08@users.noreply.github.com>
2024-04-26 10:39:22 -0700

    Create BackupProcess.java

---
+++ GPAServer/src/server/BackupProcess.java
@@ -0,0 +1,64 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.copy;
+
+/**
+ * Class that is responsible for backing up the files.
+ * 
+ */
+public class BackupProcess {
+    
+	/**
+     * Constructor that initializes the timer and calls the method responsible for backing of the data.
+     *
+     */	
+	public BackupProcess() {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					backupData();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}, 5000, 1000);
+	}
+	
+    /**
+     * Helper method for  the specified content to the disk using the identifier in the file name.
+     *
+     * @param exchange - Serializable content, String identifier
+     * @throws IOException
+     */
+	private void backupData() throws IOException {
+		System.out.println("Backup data");
+		// Source directory
+		Path sourceDir = Paths.get("C:\\Users\\sathk\\OneDrive\\Desktop\\gpacalculator\\GPAServer\\data\\");
+		// Target directory
+		Path targetDir = Paths.get("C:\\Users\\sathk\\OneDrive\\Desktop\\gpacalculator\\GPAServer\\backup\\");
+		Files.walk(sourceDir)
+		.filter(Files::isRegularFile)
+		.forEach(source -> {
+			Path target = targetDir.resolve(sourceDir.relativize(source));
+			try {
+				Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				System.err.println("Failed to copy file: " + source + " to " + target);
+				e.printStackTrace();
+			}
+		}	);
+	}
+		
+}
